e62e0c4869fb5379b7d5ffc9ad0c78485975b3c5
ckrickyh <ckricky.h@gmail.com>
2025-12-22 14:14:16 +0800

    about, index,  pages

--- pages/views.py
+++ pages/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(requests):
-  return HttpResponse("<h1>Hello World</h1>")
+  # return HttpResponse("<h1>Hello World</h1>")
+  return render(requests, 'pages/index.html')
 
 def about(request):
-  return HttpResponse("<h1>about</h1>")
+  # return HttpResponse("<h1>about</h1>")
+  return render(request, 'pages/about.html')
